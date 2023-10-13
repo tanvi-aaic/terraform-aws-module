0f5a3ff81a82b60a3f40d384e1205d2ce919db8e
Tanvi <129167954+tanvi-aaic@users.noreply.github.com>
2023-10-13 15:33:06 +0530

    Update main.tf

--- aws-ec2-module/main.tf
+++ aws-ec2-module/main.tf
@@ -2,11 +2,18 @@ provider "aws" {
   region = "us-east-1"
 }
 
-resource "aws_instance" "module-ec2" {
-  ami           = ami-053b0d53c279acc90
-  instance_type = t3a.small
-
+resource "aws_instance" "terraform-cloud" {
+  ami           = var.ami
+  instance_type = var.instance_type
   tags = {
-    Name = custom
+    Name = var.name
   }
+}
+resource "aws_eip" "example" {
+  instance = aws_instance.terraform-cloud.id
+}
+
+resource "aws_eip_association" "eip_assoc" {
+  instance_id   = aws_instance.terraform-cloud.id
+  allocation_id = aws_eip.example.id
 }
