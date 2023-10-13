86d7ecee3904eb6849d70f8022ef33bd0cabd607
Tanvi <129167954+tanvi-aaic@users.noreply.github.com>
2023-10-13 16:15:10 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,16 +1,10 @@
 //--------------------------------------------------------------------
-// Variables
-variable "module_ami" {}
-variable "module_instance_type" {}
-variable "module_name" {}
-
-//--------------------------------------------------------------------
 // Modules
 module "module" {
   source  = "app.terraform.io/AAIC-org/module/aws"
   version = "1.0.0"
 
-  ami = "${var.module_ami}"
-  instance_type = "${var.module_instance_type}"
-  name = "${var.module_name}"
+  ami = "ami-03a6eaae9938c858c"
+  instance_type = "t3a.small"
+  name = "custom-module"
 }
