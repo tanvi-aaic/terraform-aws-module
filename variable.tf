af3ca072376b881bd7f05f5faceb194b7785aa51
Tanvi <129167954+tanvi-aaic@users.noreply.github.com>
2023-10-13 21:38:15 +0530

    Update variable.tf

--- variable.tf
+++ variable.tf
@@ -1,9 +1,9 @@
 variable "ami" {
-    default = "ami-03a6eaae9938c858c"
+    
 }
 variable "instance_type" {
-    default = "t3a.small"
+
 }
 variable "name" {
-    default = "custom-module"
+    
 }
